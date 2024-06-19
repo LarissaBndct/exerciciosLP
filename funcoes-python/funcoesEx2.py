7463512249270f16cf78445cd1248e8dd491693e
LarissaBndct <larissagomes0119@gmail.com>
2024-06-19 11:16:52 -0300

    ajustes ex2

--- funcoes-python/funcoesEx2.py
+++ funcoes-python/funcoesEx2.py
@@ -2,7 +2,16 @@ def CalculoImc(altura, peso):
     imc = peso / altura**2
     print("IMC: ", imc)
 
+def Classificacao(imc):
+    if imc < 22:
+        print("Magro")
+    elif imc > 27:
+        print("Gordo")
+    else:
+        print("Ideal")
+
 a = float(input("Insira a altura: "))
 p = float(input("Insira o peso: "))
 
-CalculoImc(a, p)
+valor = CalculoImc(a, p)
+Classificacao(valor)
