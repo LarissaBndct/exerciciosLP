5fbf1a7137f25eb02fc9281bed03d7129900077d
LarissaBndct <larissagomes0119@gmail.com>
2024-06-19 12:06:01 -0300

    extenção do exercicio 4

---
+++ funcoes-python/funcoesEx4-1.py
@@ -0,0 +1,8 @@
+def ConverterF(f):
+    c = (5/9) * (f - 32)
+    return c
+
+
+for fht in range(101):
+    celcius = ConverterF(fht)
+    print(f"A temperatura {fht:.2f} F° em C° é: {celcius:.2f}")
