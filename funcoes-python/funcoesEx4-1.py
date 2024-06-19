1505abbb38422a9b430c087fd4d7b78077378de4
LarissaBndct <larissagomes0119@gmail.com>
2024-06-19 12:21:27 -0300

    extensão do ex4

--- funcoes-python/funcoesEx4-1.py
+++ funcoes-python/funcoesEx4-1.py
@@ -6,3 +6,4 @@ def ConverterF(f):
 for fht in range(101):
     celcius = ConverterF(fht)
     print(f"A temperatura {fht:.2f} F° em C° é: {celcius:.2f}")
+    #descobri que f antes da string tem a mesma funçao do $ no c# 
